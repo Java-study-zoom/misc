30da98f0e141c1a44598165a930b8610a1caa095
He Liu <liulonnie@gmail.com>
2017-10-28 14:44:47 -0700

    add support for ordered key value pair

--- psqlx/kv.go
+++ psqlx/kv.go
@@ -1,6 +1,7 @@
 package psqlx
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,25 +15,44 @@ func keyHash(k string) string {
 	return hashutil.HashStr(k)
 }
 
+// MaxKeyLen is the maximum length of a hashed KV.
+const MaxKeyLen = 255
+
 // InitKV creates a key value pair
 func InitKV(db *sqlx.DB, table string) error {
 	q := fmt.Sprintf(`create table %s (
-		k varchar(255) primary key not null,
+		k varchar(%d) primary key not null,
 		v bytea not null
-	)`, table)
+	)`, table, MaxKeyLen)
 	_, err := db.X(q)
 	return err
 }
 
-// KV defines a key value table.
+// KV defines a key value pair table.
 type KV struct {
-	db    *sqlx.DB
-	table string
+	db     *sqlx.DB
+	table  string
+	hashed bool
 }
 
-// NewKV is a generic key-value pair table.
+// NewKV connects to a generic unordered key-value pair table.
 func NewKV(db *sqlx.DB, table string) *KV {
-	return &KV{db: db, table: table}
+	return &KV{db: db, table: table, hashed: true}
+}
+
+// NewOrderedKV connects to a generic ordered key-value pair table.
+func NewOrderedKV(db *sqlx.DB, table string) *KV {
+	return &KV{db: db, table: table, hashed: false}
+}
+
+func (b *KV) mapKey(key string) (string, error) {
+	if b.hashed {
+		return keyHash(key), nil
+	}
+	if len(key) > MaxKeyLen {
+		return "", fmt.Errorf("key %q too long", key)
+	}
+	return key, nil
 }
 
 // Add adds an entry with the given key and value. The value
@@ -42,15 +62,24 @@ func (b *KV) Add(key string, v interface{}) error {
 	if err != nil {
 		return err
 	}
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
 	q := fmt.Sprintf(`insert into %s (k, v) values ($1, $2)`, b.table)
-	_, err = b.db.X(q, keyHash(key), bs)
+	_, err = b.db.X(q, mk, bs)
 	return err
 }
 
 // Remove removes the entry with the specific key.
 func (b *KV) Remove(key string) error {
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
+
 	q := fmt.Sprintf(`delete from %s where k=$1`, b.table)
-	res, err := b.db.X(q, keyHash(key))
+	res, err := b.db.X(q, mk)
 	if err != nil {
 		return err
 	}
@@ -67,8 +96,12 @@ func (b *KV) Remove(key string) error {
 
 // Get gets the value and json marshals it into v.
 func (b *KV) Get(key string, v interface{}) error {
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
 	q := fmt.Sprintf(`select v from %s where k=$1`, b.table)
-	row := b.db.Q1(q, keyHash(key))
+	row := b.db.Q1(q, mk)
 	var bs []byte
 	if has, err := row.Scan(&bs); err != nil {
 		return err
@@ -81,6 +114,10 @@ func (b *KV) Get(key string, v interface{}) error {
 
 // Emplace sets the value for a particular key. Creates the key if not exist.
 func (b *KV) Emplace(key string, v interface{}) error {
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
 	bs, err := json.Marshal(v)
 	if err != nil {
 		return err
@@ -89,25 +126,33 @@ func (b *KV) Emplace(key string, v interface{}) error {
 		insert into %s (k, v) values ($1, $2)
 		on conflict (k) do update set v=excluded.v
 	`, b.table)
-	_, err = b.db.X(q, keyHash(key), bs)
+	_, err = b.db.X(q, mk, bs)
 	return err
 }
 
 // AppendBytes appends the value to the existing value of a particular key.
 // Creates the key if not exist.
 func (b *KV) AppendBytes(key string, bs []byte) error {
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
 	q := fmt.Sprintf(`
 		insert into %s (k, v) values ($1, $2)
 		on conflict (k) do update set v = v || excluded.v
 	`, b.table)
-	_, err := b.db.X(q, keyHash(key), bs)
+	_, err = b.db.X(q, mk, bs)
 	return err
 }
 
 // SetBytes updates the value bytes of a particular key.
 func (b *KV) SetBytes(key string, bs []byte) error {
+	mk, err := b.mapKey(key)
+	if err != nil {
+		return err
+	}
 	q := fmt.Sprintf(`update %s set v=$1 where k=$2`, b.table)
-	res, err := b.db.X(q, bs, keyHash(key))
+	res, err := b.db.X(q, bs, mk)
 	if err != nil {
 		return err
 	}
@@ -141,17 +186,20 @@ var ErrCancel = errors.New("operation cancelled")
 func (b *KV) Mutate(
 	k string, v interface{}, f func(v interface{}) error,
 ) error {
+	mk, err := b.mapKey(k)
+	if err != nil {
+		return err
+	}
+
 	tx, err := b.db.Begin()
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
 
-	hash := keyHash(k)
-
 	var bs []byte
 	q := fmt.Sprintf(`select v from %s where k=$1`, b.table)
-	row := tx.Q1(q, hash)
+	row := tx.Q1(q, mk)
 	if has, err := row.Scan(&bs); err != nil {
 		return err
 	} else if !has {
@@ -176,7 +224,7 @@ func (b *KV) Mutate(
 	}
 
 	q = fmt.Sprintf(`update %s set v=$1 where k=$2`, b.table)
-	res, err := tx.X(q, bs, hash)
+	res, err := tx.X(q, bs, mk)
 	if err != nil {
 		return err
 	}
@@ -193,10 +241,27 @@ func (b *KV) Mutate(
 	return tx.Commit()
 }
 
+func iterRows(rows *sql.Rows, it Iter) error {
+	for rows.Next() {
+		var bs []byte
+		if err := rows.Scan(&bs); err != nil {
+			return err
+		}
+
+		entry := it.Make()
+		if err := json.Unmarshal(bs, entry); err != nil {
+			return err
+		}
+		if err := it.Do(entry); err != nil {
+			return err
+		}
+	}
+
+	return rows.Close()
+}
+
 // Walk iterates through all items in the key value store.
-func (b *KV) Walk(
-	makeFunc func() interface{}, f func(v interface{}) error,
-) error {
+func (b *KV) Walk(it Iter) error {
 	q := fmt.Sprintf(`select k, v from %s order by k`, b.table)
 	rows, err := b.db.Q(q)
 	if err != nil {
@@ -204,20 +269,25 @@ func (b *KV) Walk(
 	}
 	defer rows.Close()
 
-	for rows.Next() {
-		var bs []byte
-		if err := rows.Scan(&bs); err != nil {
-			return err
-		}
+	return iterRows(rows, it)
+}
 
-		entry := makeFunc()
-		if err := json.Unmarshal(bs, entry); err != nil {
-			return err
-		}
-		if err := f(entry); err != nil {
-			return err
-		}
+// WalkPartial walks thorugh the items at offset with at most n items.
+func (b *KV) WalkPartial(offset, n uint64, desc bool, it Iter) error {
+	if b.hashed {
+		return fmt.Errorf("cannot partial walk over a hashed table")
 	}
+	q := fmt.Sprintf(
+		`select k, v from %s order by k offset ? limit ?`, b.table,
+	)
+	if desc {
+		q = q + " desc"
+	}
+	rows, err := b.db.Q(q, offset, n)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-	return rows.Close()
+	return iterRows(rows, it)
 }
