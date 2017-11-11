76181db56305d282d326cd94f1391846682e326e
He Liu <liulonnie@gmail.com>
2017-11-11 15:43:39 -0800

    add more test case for split

--- pathutil/split_test.go
+++ pathutil/split_test.go
@@ -2,9 +2,35 @@ package pathutil
 
 import (
 	"testing"
+
+	"reflect"
 )
 
 func TestSplit(t *testing.T) {
+	for _, test := range []struct {
+		p     string
+		parts []string
+	}{
+		{"a", []string{"a"}},
+		{"a/b", []string{"a", "b"}},
+		{"shanhu.io/smlvm", []string{"shanhu.io", "smlvm"}},
+	} {
+		parts, err := Split(test.p)
+		if err != nil {
+			t.Errorf("Split(%q) got error: %s", test.p, err)
+			continue
+		}
+
+		if !reflect.DeepEqual(parts, test.parts) {
+			t.Errorf(
+				"Split(%q) want %v got %v",
+				test.p, test.parts, parts,
+			)
+		}
+	}
+}
+
+func TestSplitInvalidPath(t *testing.T) {
 	for _, p := range []string{
 		"",
 		"/x",
