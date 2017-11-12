d73c78b5ba6b75fc60fa76d7329461bdee5ca9aa
Lonnie Liu <liulonnie@gmail.com>
2017-11-12 11:40:50 -0800

    adding class for kv

--- psqlx/kv_init.go
+++ psqlx/kv_init.go
@@ -6,12 +6,26 @@ import (
 	"shanhu.io/misc/sqlx"
 )
 
+// MaxClassLen is the maximum length of the class string of a hashed KV.
+const MaxClassLen = 255
+
 // InitKV creates a key value pair
 func InitKV(db *sqlx.DB, table string) error {
 	q := fmt.Sprintf(`create table %s (
 		k varchar(%d) primary key not null,
+		c varchar(%d) not null,
 		v bytea not null
-	)`, table, MaxKeyLen)
+	)`, table, MaxKeyLen, MaxClassLen)
+	_, err := db.X(q)
+	return err
+}
+
+// KVAddClassColumn adds a class column for the KV table.
+func KVAddClassColumn(db *sqlx.DB, table string) error {
+	q := fmt.Sprintf(
+		`alter table %s add column c varchar(%d) not null`,
+		table, MaxClassLen,
+	)
 	_, err := db.X(q)
 	return err
 }
--- psqlx/kv_key.go
+++ psqlx/kv_key.go
@@ -1,12 +1,38 @@
 package psqlx
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
+
 	"shanhu.io/misc/hashutil"
+	"shanhu.io/misc/pathutil"
 )
+
+// MaxKeyLen is the maximum length of a hashed KV.
+const MaxKeyLen = 255
 
 func keyHash(k string) string {
 	return hashutil.HashStr(k)
 }
 
-// MaxKeyLen is the maximum length of a hashed KV.
-const MaxKeyLen = 255
+func kvMapKey(key string, hashed bool) (string, error) {
+	if hashed {
+		return keyHash(key), nil
+	}
+	if len(key) > MaxKeyLen {
+		return "", fmt.Errorf("key %q too long", key)
+	}
+	return key, nil
+}
+
+func kvResError(res sql.Result, key string) error {
+	if n, err := res.RowsAffected(); err != nil {
+		return err
+	} else if n == 0 {
+		return pathutil.NotExist(key)
+	} else if n == 1 {
+		return errors.New("multiple rows affected")
+	}
+	return nil
+}
