dcfe004148595fe7f1409b3d8167ade60ff04434
Lonnie Liu <liulonnie@gmail.com>
2017-12-24 16:11:19 -0800

    more relative path stuff

--- pathutil/relative.go
+++ pathutil/relative.go
@@ -4,8 +4,10 @@ import (
 	"strings"
 )
 
-// Relative returns the relative path of full to base.
-func Relative(base, full string) string {
+// DotRelative returns the relative path of full to base.
+// The return value starts with a dot if base is a parent of full.
+// It returns full unchanged if base is not a parent of full.
+func DotRelative(base, full string) string {
 	if base == full {
 		return "."
 	}
@@ -13,4 +15,18 @@ func Relative(base, full string) string {
 		return "./" + strings.TrimPrefix(full, base+"/")
 	}
 	return full
+}
+
+// Relative returns the relative path of full to base.
+// It returns the relative path if base is a parent of full.
+// It returns a single dot if base and full are the same.
+// It retunrs an empty string if base is not a parent of full.
+func Relative(base, full string) string {
+	if base == full {
+		return "."
+	}
+	if strings.HasPrefix(full, base+"/") {
+		return strings.TrimPrefix(full, base+"/")
+	}
+	return ""
 }
--- pathutil/relative_test.go
+++ pathutil/relative_test.go
@@ -9,16 +9,37 @@ func TestRelative(t *testing.T) {
 		base, full string
 		want       string
 	}{
+		{"a", "b", ""},
+		{"a", "a/b", "b"},
+		{"a", "a", "."},
+		{"a", "a/b/c", "b/c"},
+		{"a", "ab/c", ""},
+	} {
+		got := Relative(test.base, test.full)
+		if got != test.want {
+			t.Errorf(
+				"Relative(%q, %q), want %q, got %q",
+				test.base, test.full, test.want, got,
+			)
+		}
+	}
+}
+
+func TestDotRelative(t *testing.T) {
+	for _, test := range []struct {
+		base, full string
+		want       string
+	}{
 		{"a", "b", "b"},
 		{"a", "a/b", "./b"},
 		{"a", "a", "."},
 		{"a", "a/b/c", "./b/c"},
 		{"a", "ab/c", "ab/c"},
 	} {
-		got := Relative(test.base, test.full)
+		got := DotRelative(test.base, test.full)
 		if got != test.want {
 			t.Errorf(
-				"Relative(%q, %q), want %q, got %q",
+				"DotRelative(%q, %q), want %q, got %q",
 				test.base, test.full, test.want, got,
 			)
 		}
