b1664d45bf384192a955c0d7f071bc730e14a57d
Lonnie Liu <liulonnie@gmail.com>
2017-12-29 20:40:00 -0800

    tempfile test

---
+++ tempfile/file_test.go
@@ -0,0 +1,124 @@
+package tempfile
+
+import (
+	"testing"
+
+	"io/ioutil"
+	"os"
+	"reflect"
+)
+
+func testFileExist(t *testing.T, name string) bool {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	t.Fatal(err)
+	return false
+}
+
+func TestFileReadBack(t *testing.T) {
+	ne := func(err error) {
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	f, err := NewFile("", "tempfile")
+	ne(err)
+	defer f.CleanUp()
+
+	msg := []byte("hello")
+
+	_, err = f.Write(msg)
+	ne(err)
+
+	ne(f.Reset())
+
+	readBack, err := ioutil.ReadAll(f)
+	ne(err)
+
+	if !reflect.DeepEqual(msg, readBack) {
+		t.Errorf("want %q, got %q", string(msg), string(readBack))
+	}
+}
+
+func TestFileCleanUp(t *testing.T) {
+	ne := func(err error) {
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	testFunc := func() string {
+		f, err := NewFile("", "tempfile")
+		ne(err)
+		defer f.CleanUp()
+
+		return f.Name
+	}
+
+	f := testFunc()
+	if testFileExist(t, f) {
+		t.Errorf("test file %q still exist", f)
+	}
+}
+
+func TestFileRename(t *testing.T) {
+	ne := func(err error) {
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	testFunc := func() (string, string) {
+		f, err := NewFile("", "tempfile")
+		ne(err)
+		defer f.CleanUp()
+
+		f.SkipCleanUp = true
+
+		target, err := NewFile("", "tempfile")
+		ne(err)
+		defer target.CleanUp()
+
+		ne(f.Rename(target.Name))
+
+		return f.Name, target.Name
+	}
+
+	f1, f2 := testFunc()
+	if testFileExist(t, f1) {
+		t.Errorf("test file1 %q still exist", f1)
+	}
+	if testFileExist(t, f2) {
+		t.Errorf("test file2 %q still exist", f2)
+	}
+}
+
+func TestFileSkipCleanUp(t *testing.T) {
+	ne := func(err error) {
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	testFunc := func() string {
+		f, err := NewFile("", "tempfile")
+		ne(err)
+		f.SkipCleanUp = true
+		defer f.CleanUp()
+
+		return f.Name
+	}
+
+	f := testFunc()
+	if !testFileExist(t, f) {
+		t.Errorf("test file %q should exist", f)
+	}
+
+	ne(os.Remove(f))
+}
