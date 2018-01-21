5bfd932b0b369c69ca6adc728ece9ae78ab728fd
Lonnie Liu <liulonnie@gmail.com>
2018-01-21 09:38:49 -0800

    osutil read token

---
+++ osutil/read_token_file.go
@@ -0,0 +1,28 @@
+package osutil
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// ReadTokenFile reads a token string from a file.
+func ReadTokenFile(f string) (string, error) {
+	bs, err := ioutil.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(bs)), nil
+}
+
+// ReadOptionalTokenFile reads an optional token file.
+func ReadOptionalTokenFile(f string) (string, bool, error) {
+	ret, err := ReadTokenFile(f)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", false, nil
+		}
+		return "", false, err
+	}
+	return ret, true, nil
+}
