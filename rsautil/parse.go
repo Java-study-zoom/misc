71b3863b6d0604f7f41c969670b15bc7d73878cd
He Liu <liulonnie@gmail.com>
2018-05-03 04:55:07 -0700

    add rsa key parsing

--- rsautil/parse.go
+++ rsautil/parse.go
@@ -1,1 +1,22 @@
 package rsautil
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+)
+
+// ParsePrivateKey parses the PEM encoded RSA private key.
+func ParsePrivateKey(bs []byte) (*rsa.PrivateKey, error) {
+	b, _ := pem.Decode(bs)
+	if b == nil {
+		return nil, fmt.Errorf("pem decode failed")
+	}
+
+	if !x509.IsEncryptedPEMBlock(b) {
+		return nil, fmt.Errorf("key is encrypted")
+	}
+
+	return x509.ParsePKCS1PrivateKey(b.Bytes)
+}
