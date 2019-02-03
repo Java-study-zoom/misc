2534789c94a2993f8e1baff3fad643c337c83cb6
Leon <leonreed@users.noreply.github.com>
2019-02-02 18:08:31 -0800

    flagutil

--- flagutil/flag_factory.go
+++ flagutil/flag_factory.go
@@ -9,6 +9,11 @@ type FlagFactory struct {
 	name string
 }
 
+// NewFactory creates a new flag factory with the given name.
+func NewFactory(name string) *FlagFactory {
+	return &FlagFactory{name: name}
+}
+
 // ParseArgs parses the args using the given FlagSet, and returns
 // the parsed args that does not include flags.
 func ParseArgs(s *flag.FlagSet, args []string) []string {
@@ -16,13 +21,27 @@ func ParseArgs(s *flag.FlagSet, args []string) []string {
 	return s.Args()
 }
 
-// Make creates a new flag set.
-func (f *FlagFactory) Make() *flag.FlagSet {
-	return flag.NewFlagSet(f.name, flag.ExitOnError)
+// New creates a new flag set.
+func (f *FlagFactory) New() *FlagSet {
+	set := flag.NewFlagSet(f.name, flag.ExitOnError)
+	return &FlagSet{
+		FlagSet: set,
+	}
 }
 
 // PlainArgs parse the args with no flags.
 func (f *FlagFactory) PlainArgs(args []string) []string {
-	flags := f.Make()
-	return ParseArgs(flags, args)
+	flags := f.New()
+	return flags.ParseArgs(args)
+}
+
+// FlagSet extends the *flag.FlagSet with some more common helper functions.
+type FlagSet struct {
+	*flag.FlagSet
+}
+
+// ParseArgs parses the args and returns the parsed args that does not include
+// flags.
+func (s *FlagSet) ParseArgs(args []string) []string {
+	return ParseArgs(s.FlagSet, args)
 }
