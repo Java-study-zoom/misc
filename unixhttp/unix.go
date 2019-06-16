c2b95d75ac586f0842d36e3e6d5e67b5ebb9bd1d
Lonnie Liu <liulonnie@gmail.com>
2019-06-15 20:37:59 -0700

    provides unix domain socket util

---
+++ unixhttp/unix.go
@@ -0,0 +1,41 @@
+package unixhttp
+
+import (
+	"net"
+	"net/http"
+	"os"
+
+	"shanhu.io/misc/osutil"
+)
+
+// Listen creates a listener at the given unix domain socket address.
+func Listen(p string) (net.Listener, error) {
+	exist, err := osutil.IsSock(p)
+	if err != nil {
+		return nil, err
+	}
+
+	if exist {
+		if err := os.Remove(p); err != nil {
+			return nil, err
+		}
+	}
+	lis, err := net.ListenUnix("unix", &net.UnixAddr{
+		Name: p,
+		Net:  "unix",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return lis, nil
+}
+
+// ListenAndServe listens and serves at the given unix domain socket
+// path.
+func ListenAndServe(p string, h http.Handler) error {
+	lis, err := Listen(p)
+	if err != nil {
+		return err
+	}
+	return http.Serve(lis, h)
+}
