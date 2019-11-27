79c1ebbb93d8d7b95dd534977ef3f7732daf6c6d
Lonnie Liu <liulonnie@gmail.com>
2019-11-27 17:55:26 +0800

    add is regular

--- osutil/file_exist.go
+++ osutil/file_exist.go
@@ -4,7 +4,7 @@ import (
 	"os"
 )
 
-// Exist checks if a file exists.
+// Exist checks if a path exists.
 func Exist(path string) (bool, error) {
 	if _, err := os.Stat(path); err != nil {
 		if os.IsNotExist(err) {
@@ -13,4 +13,16 @@ func Exist(path string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+// IsRegular checks if a path is a regular file.
+func IsRegular(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return stat.Mode().IsRegular(), nil
 }
