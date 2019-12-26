1411eee908849825abf4f512e9a1a189a58db4cf
Lonnie Liu <liulonnie@gmail.com>
2019-12-26 15:24:07 -0800

    add jsonutil.Format function

--- jsonutil/print.go
+++ jsonutil/print.go
@@ -1,6 +1,7 @@
 package jsonutil
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,9 +9,11 @@ import (
 	"os"
 )
 
+const formatIndent = "  "
+
 // Fprint pretty prints a JSON data blob into a writer.
 func Fprint(w io.Writer, v interface{}) error {
-	bs, err := json.MarshalIndent(v, "", "  ")
+	bs, err := json.MarshalIndent(v, "", formatIndent)
 	if err != nil {
 		return err
 	}
@@ -26,4 +29,13 @@ func Print(v interface{}) {
 	if err := Fprint(os.Stdout, v); err != nil {
 		log.Println(err)
 	}
+}
+
+// Format pretty formats JSON data bytes.
+func Format(bs []byte) ([]byte, error) {
+	out := new(bytes.Buffer)
+	if err := json.Indent(out, bs, "", formatIndent); err != nil {
+		return nil, err
+	}
+	return out.Bytes(), nil
 }
