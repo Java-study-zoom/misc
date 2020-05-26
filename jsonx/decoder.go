02fab7081db80623155736f92a50a19e29c54b02
Leon <leonreed@users.noreply.github.com>
2020-05-25 18:15:49 -0700

    add maybe for read file.

--- jsonx/decoder.go
+++ jsonx/decoder.go
@@ -27,7 +27,8 @@ func (d *Decoder) More() bool {
 	return !(d.p.See(lexing.EOF) || d.p.Token() == nil)
 }
 
-// Decode decodes a JSON value from the parser.
+// Decode decodes a JSON value from the parser. When there is
+// error on parsing JSONx, v is always unchanged.
 func (d *Decoder) Decode(v interface{}) []*lexing.Error {
 	value := parseValue(d.p)
 	if errs := d.p.Errs(); errs != nil {
@@ -47,7 +48,8 @@ func (d *Decoder) Decode(v interface{}) []*lexing.Error {
 	return nil
 }
 
-// Unmarshal unmarshals a value into a JSON object.
+// Unmarshal unmarshals a value into a JSON object. When there is an error on
+// parsing JSONx, v is always unchagned.
 func Unmarshal(bs []byte, v interface{}) error {
 	dec := NewDecoder(bytes.NewReader(bs))
 	if errs := dec.Decode(v); errs != nil {
@@ -66,4 +68,20 @@ func ReadFile(file string, v interface{}) error {
 		return err
 	}
 	return Unmarshal(bs, v)
+}
+
+// ReadFileMaybeJSON reads a file that might be JSONx or JSON.
+func ReadFileMaybeJSON(file string, v interface{}) error {
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	if err := Unmarshal(bs, v); err != nil {
+		// JSONx fails to decode, maybe it is plain JSON?
+		if json.Unmarshal(bs, v) == nil {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
