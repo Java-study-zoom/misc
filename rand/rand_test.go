5f6c7f4cb8d6374c11e614e91a9a55d5d755c8d5
Lonnie Liu <liulonnie@gmail.com>
2020-11-08 18:24:56 -0800

    add/refactor rand tests

--- rand/rand_test.go
+++ rand/rand_test.go
@@ -3,14 +3,15 @@ package rand
 import (
 	"testing"
 
-	"reflect"
+	"bytes"
+	"strings"
 )
 
 func TestBytes(t *testing.T) {
 	bs1 := Bytes(8)
 	bs2 := Bytes(8)
 
-	if reflect.DeepEqual(bs1, bs2) {
+	if bytes.Equal(bs1, bs2) {
 		t.Errorf("not so random: %v == %v", bs1, bs2)
 	}
 }
@@ -18,6 +19,17 @@ func TestBytes(t *testing.T) {
 func TestLowerLetters(t *testing.T) {
 	s1 := LowerLetters(16)
 	s2 := LowerLetters(16)
+	if s1 == s2 {
+		t.Errorf("not so random: %q == %q", s1, s2)
+	}
+	if strings.ToLower(s1) != s1 {
+		t.Errorf("contains non-lower case: %q", s1)
+	}
+}
+
+func TestLetters(t *testing.T) {
+	s1 := Letters(16)
+	s2 := Letters(16)
 	if s1 == s2 {
 		t.Errorf("not so random: %q == %q", s1, s2)
 	}
