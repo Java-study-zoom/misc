639699c351781694fecc84d083520c5aafcc6904
Leon <leonreed@users.noreply.github.com>
2020-11-23 19:56:06 -0800

    generating random id

---
+++ idutil/time_id.go
@@ -0,0 +1,12 @@
+package idutil
+
+import (
+	"time"
+
+	"shanhu.io/misc/rand"
+)
+
+// TimeRandID generates a timestamp based unique ID.
+func TimeRandID(t time.Time, nbytes int) string {
+	return t.Format("20060102-150405") + "-" + rand.HexBytes(nbytes)
+}
